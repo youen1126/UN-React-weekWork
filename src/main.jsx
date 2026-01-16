36dc026a5ff2519123bbf3c5be16dead0d436af9
UN <youen1126@gmail.com>
2026-01-16 15:24:32 +0800

    整理好專案檔

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-//sass 匯入練習
-import './assets/exportJs.js'
-
-//預設匯出練習
-//import exportJS from './assets/exportJs.js';
-//import MyApp from './MyApp.jsx';
-import AppUseState from './AppUseState.jsx';
-import AppUseEffect from './AppUseEffect.jsx';
-import AppUseRef from './AppUseRef.jsx';
-
-
-//exportJS();
-
-//具名匯出練習
-
-// import { myName, fnMyName } from './assets/exportJs.js'
-
-// console.warn(myName);
-// fnMyName();
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -29,6 +10,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   //嚴謹模式<StrictMode>
 
-  <AppUseRef />
+  <App />
 
 )
