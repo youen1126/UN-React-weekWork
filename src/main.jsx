a06b0bd31990610cd2ef69b100e443a5d35c512e
UN <youen1126@gmail.com>
2026-01-16 17:38:19 +0800

    作業week2完成

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-
+// 載入 bootstrap 的 css 與 js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import App from './App.jsx'
+import Week2 from './Week2.jsx'
 
 createRoot(document.getElementById('root')).render(
   //嚴謹模式<StrictMode>
+  <Week2 />
 
-  <App />
 
 )
