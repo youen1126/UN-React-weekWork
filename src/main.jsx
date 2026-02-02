870e0f11d7feb91246e5b76a5c284e3876b974fc
UN <youen1126@gmail.com>
2026-02-02 16:45:32 +0800

    feat:路由設定

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+
+
 // 載入 bootstrap 的 css 與 js
 //import './assets/all.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import App from './App.jsx'
-import Week2 from './Week2.jsx'
-import Week3 from './Week3.jsx'
-//import Daily from './practise/Daily.jsx'
-import Week4 from './Week4.jsx'
+
+import Week5 from './Week5.jsx';
+//import Week4 from './weekWork/Week4.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   //嚴謹模式<StrictMode>
-  <Week4 />
+  <Week5 />
 
 )
