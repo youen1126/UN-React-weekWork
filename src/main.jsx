f00d46aac72e7b5ac78076c86a414b995608d31e
UN <youen1126@gmail.com>
2026-02-02 17:07:05 +0800

    fix:嘗試修衝突

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
+import App from './App.jsx'
+import Week2 from './Week2.jsx'
 import Week5 from './Week5.jsx';
 //import Week4 from './weekWork/Week4.jsx';
 
